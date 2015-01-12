5cb4aa8216f1877edb6670ee6fc5525824cd1c8c
Mark Inns <mark@markinns.com>
2015-01-12 16:21:03 +0000

    Create functions.php
    This document provides some helper functions, which are used in custom template functions files. Others override WordPress defaults to change core functionality.

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Wordpress Functions
+ *
+ * This document provides some helper functions, which are used
+ * in custom template functions files. Others override WordPress 
+ * defaults to change core functionality.
+ */
+ 
+/**
+ * Disable WordPress auto-paragraphing
+ */
+/*
+remove_filter('the_content', 'wpautop');
+*/
